d602f1322b5b0006a9e304ebb2aa28a02cf85dfd
a22wilmccash <121022402+a22wilmccash@users.noreply.github.com>
2022-12-19 11:58:46 +0100

    Update TESTM05_19DIC.java

--- TESTM05_19DIC/src/testm05_19dic/TESTM05_19DIC.java
+++ TESTM05_19DIC/src/testm05_19dic/TESTM05_19DIC.java
@@ -3,6 +3,12 @@ package testm05_19dic;
 public class TESTM05_19DIC {
 
     public static void main(String[] args) {
+        
+        int i = 0;
+            
+        while (i<100){
+            System.out.print("valor de i:" + 1)
+            }
 
     }
     
